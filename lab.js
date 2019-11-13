2b441a926b56ac255070c3c9b1c03ac411625008
karenm09 <karenm929@hotmail.com>
2019-11-13 00:52:47 -0500

    the strikethrough works on the list item and value clears after sumbit

--- lab.js
+++ lab.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     let form = document.querySelector('form')
     form.addEventListener('submit', (event) => {
+        event.preventDefault()
         let input = document.querySelector('#input')
-        if(!input.value){
+        if (!input.value) {
 
             let error = document.createElement('p')
-           error.innerText = 'error. To Do cannot be empty'
-          
-
+            error.innerText = 'error. To Do cannot be empty'
+            
+            
             document.body.appendChild(error)
-
-
-        }else{
-            let inputStr = document.createElement('li')
-            inputStr.innerText = input.value
-            document.body.appendChild(inputStr)
+            
+            
+        } else {
+            let todo = document.querySelector("ul");
+            let inputStr = document.createElement('li');
+            inputStr.innerText = input.value;
+            input.value =""
+            inputStr.addEventListener('click', (e) => {
+                e.target.style.textDecoration = "line-through";
+            })
+            todo.appendChild(inputStr)
         }
 
 
-        event.preventDefault()
-               
-       
     })
-
 
 })
